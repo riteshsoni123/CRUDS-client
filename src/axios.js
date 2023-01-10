90dea51c96881e116b8aa25432b04e425b39d522
riteshsoni123 <comedybuddy1@gmail.com>
2023-01-10 19:13:45 +0530

    made changes in axios

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,12 @@
+// import axios from "axios";
+// const instance = axios.create({
+//   baseURL: "http://localhost:10000",
+// });
+
+// https://demo-kf7p.onrender.com/
+
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:10000",
+  baseURL: "https://demo-kf7p.onrender.com",
 });
-
 export default instance;
